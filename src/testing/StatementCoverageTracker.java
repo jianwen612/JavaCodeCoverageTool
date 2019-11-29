f93591857d5dfc2a9b351e29c103d192515d9727
jianwen612 <djw612@outlook.com>
2019-11-28 18:24:19 -0600

    finish runtime coverage

--- src/testing/StatementCoverageTracker.java
+++ src/testing/StatementCoverageTracker.java
@@ -1,4 +1,8 @@
 package testing;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -6,29 +10,57 @@ public class StatementCoverageTracker {
     // Maps filenames and line numbers to true (executed) or false (not executed).
     private static Map<String, Map<Integer, Boolean>> coverage =
             new HashMap<String, Map<Integer, Boolean>>();
-
+    private static final String classpath="/Users/jianwendong/softwaretesting/JavaCodeCoverageTool/classfile/";
+    private static final String fileName="coverage.txt";
+    private static String report="";
     // Serializes coverage in some format; we'll revisit this.
-    public static void writeCoverageToFile() { }
+    private static void writeCoverageToFile() throws IOException {
+        countAllCoveredLine();
+        File out=new File(classpath+fileName); //store to classpath/filename
+//        System.out.println(out.getAbsolutePath());
+        if(!out.exists()) {
+            out.createNewFile();
+        } else {
+            out.delete();
+            out.createNewFile();
+        }
+
+//            if(!out.getParentFile().exists()){
+//                out.getParentFile().mkdirs();
+//            }
+
+        //2：准备输出流
+        Writer writer = new FileWriter(out);
+        writer.write(report);
+        writer.close();
+
+    }
 
     public static boolean markExecuted(String filename, int line) {
         if (!coverage.containsKey(filename)) {
             coverage.put(filename, new HashMap<Integer, Boolean>());
         }
         coverage.get(filename).put(line, true);
+        try{
+            writeCoverageToFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
         return false;
     }
     public static  int countAllCoveredLine(){
         int result=0;
-        System.out.println("Coverage: ");
+        report="Coverage: \n";
 
         for(String filename:coverage.keySet()){
-            System.out.println("    In file "+filename+":");
+            report=report+"    In file "+filename+":\n";
             for(Integer line:coverage.get(filename).keySet()){
-                System.out.println("        Line "+line+" is covered");
+                report=report+"        Line "+line+" is covered\n";
                 result++;
             }
         }
-        System.out.println("Totally " +result+" lines"+ " covered");
+        report+="Totally " +result+" lines"+ " covered";
         return result;
     }
 }
